e786146135befab477da58173e14947a047aabb8
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-04 12:26:05 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
 });
