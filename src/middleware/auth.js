b145fbf2cf9057b4c90a07a80c4ca85984bc2cca
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-13 14:26:02 -0600

    Delete src directory

--- src/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-import basicAuth from 'express-basic-auth';
-import { config } from '../config/index.js';
-
-export const adminAuth = basicAuth({
-  users: { [config.admin.username]: config.admin.password },
-  challenge: true
-});
