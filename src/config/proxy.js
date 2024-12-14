16a75ca1f66ccf2601f6238027cd56e955e87352
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-13 18:21:57 -0600

    Delete src directory

--- src/config/proxy.js
+++
@@ -1,5 +0,0 @@
-export const proxyConfig = {
-  server: 'socks5://45.166.110.64:50101',
-  username: 'arturojkl',
-  password: 'HQKxDbtCiC'
-};
--- src/utils/urlUtils.js
+++
@@ -1,8 +0,0 @@
-export function getServiceDomain(url) {
-  try {
-    return new URL(url).hostname;
-  } catch (error) {
-    console.error('Invalid URL:', error);
-    return null;
-  }
-}
