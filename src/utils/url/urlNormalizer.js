6984cf41f3c3efea9995f7d56e54836bacf5ba85
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2024-12-13 23:24:50 -0600

    Delete src directory

--- src/utils/url/urlNormalizer.js
+++
@@ -1,9 +0,0 @@
-export function normalizeUrl(url) {
-  try {
-    const urlObj = new URL(url.startsWith('//') ? `https:${url}` : url);
-    return urlObj.pathname + urlObj.search + urlObj.hash;
-  } catch (error) {
-    console.error('Error normalizing URL:', error);
-    return url.startsWith('/') ? url : `/${url}`;
-  }
-}
